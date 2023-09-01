a5bd72d76e2e6171c100d1484365675aa50aaf54
shaileshshail <109917260+shaileshshail@users.noreply.github.com>
2023-09-01 19:29:51 +0530

    Initial setup

---
+++ script.js
@@ -0,0 +1,10 @@
+const toggleBtn= document.getElementsByClassName('toggle-btn')[0]
+const navbarLinks= document.getElementsByClassName('navbar-links')[0]
+
+toggleBtn.addEventListener('click',()=>{
+    navbarLinks.classList.toggle('active');
+})
+
+navbarLinks.addEventListener('click',()=>{
+    navbarLinks.classList.toggle('active');
+})
